e691b3f9a3a957dd913c3f40a0cda751a0f0a7c6
Spencer Eldred <eldredspencer@gmail.com>
2014-07-18 15:11:39 -0600

    Add flash error message, 140 character max

--- app.rb
+++ app.rb
@@ -9,10 +9,23 @@ class App < Sinatra::Application
 
   def initialize
     super
+    @database_connection ||= DatabaseConnection.establish(ENV["RACK_ENV"])
   end
 
   get "/" do
+    @messages = @database_connection.sql("SELECT * FROM messages")
+
     erb :home
+  end
+
+  post "/message" do
+    message = params[:message]
+    if message.length <= 140
+      @database_connection.sql("INSERT INTO messages (message) VALUES ('#{message}')")
+    else
+      flash[:notice] = ""[:notice] = "Error: restrict your message to only 140 characters."
+    end
+    redirect "/"
   end
 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,8 +2,31 @@ require 'rspec'
 require 'capybara'
 
 feature "home page" do
-  scenario "says Howdy" do
+  scenario "has content" do
     visit '/'
-    expect(page).to have_content("Howdy Doody")
+    expect(page).to have_title("Message")
+    expect(page).to have_content("Message Roullete")
+  end
+
+  scenario "as a user, I can submit a message" do
+    visit '/'
+
+    expect(page).to have_content("Message:")
+    fill_in "message", :with => "Hello Everyone!"
+
+    click_button "Submit"
+
+    expect(page).to have_content("Hello Everyone!")
+  end
+
+  scenario "As a user, I see an error message if I enter a message > 140 characters" do
+    visit '/'
+
+    expect(page).to have_content("Message:")
+    fill_in "message", :with => "a" * 141
+
+    click_button "Submit"
+
+    expect(page).to have_content("Error: restrict your message to only 140 characters.")
   end
 end
